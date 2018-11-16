14f54573c30a759512268f92a616d9434c1f40e7
prospere <prospere@i5-68.ens-epua.univ-savoie.fr>
2018-11-16 08:18:24 +0100

    Light changes

--- java_serv/src/mainRobotinoTest.java
+++ java_serv/src/mainRobotinoTest.java
@@ -1,9 +1,10 @@
-/*import java.io.BufferedReader;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.concurrent.TimeUnit;*/
+import java.util.concurrent.TimeUnit;
 
 public class mainRobotinoTest {
 
@@ -11,14 +12,14 @@ public class mainRobotinoTest {
 		// TODO Auto-generated constructor stub
 	}
 	public static void main(String[] args) {
-		int port=50007;
+		int port=50008;
 		String ipServer="193.48.125.70";
 		SocketRobotino socketRobotino = new SocketRobotino(ipServer, port);
 		new Thread(socketRobotino).start();
-		/*PrintWriter out;
+		PrintWriter out;
 		BufferedReader in;
 		Socket clientSocket;
-		String ipServer="192.168.56.1";//iplocal
+		//String ipServer="192.168.56.1";//iplocal
 		//ipServer="193.48.125.70";
 		//ipServer="193.48.125.219";
 		//new Thread(new Client(ipServer,port,"C1")).start();
@@ -44,6 +45,5 @@ public class mainRobotinoTest {
 			e.printStackTrace();
 		}
 		//System.out.println(""+this.nom+"\tgetOutputStream: "+clientSocket.getOutputStream());
-		 */
 	}
 }

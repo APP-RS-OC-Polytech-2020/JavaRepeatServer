372e29b6fdcd5d34d7e7fd2b95a88c7c210d1a98
prospere <prospere@i5-68.ens-epua.univ-savoie.fr>
2018-12-18 08:43:09 +0100

    Merging

---
+++ java_serv/src/ConnexionWebcam.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.net.Socket;
+
+/**
+ * Classe qui doit faire une connection a la webcam, et qui doit redistribuer a tout les clients.
+ * @author prospere
+ *
+ */
+public class ConnexionWebcam implements Runnable {
+
+	public ConnexionWebcam(ServerRobotino serverRobotino, Socket socketClient, String firstLine, BufferedReader in){
+		
+	}
+	
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
